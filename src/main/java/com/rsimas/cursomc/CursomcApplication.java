c9fc76c9a858f96e0b2932139cd96e6e1da3ece1
Ricardo Simas <Rsimas@unigranrio.br>
2019-11-29 19:38:21 -0300

    Projeto criado.

---
+++ src/main/java/com/rsimas/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.rsimas.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
