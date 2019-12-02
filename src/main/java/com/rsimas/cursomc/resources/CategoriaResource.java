9593db5391e0b4cb295c6551d1c18a6c4c9422ec
Ricardo Simas <Rsimas@unigranrio.br>
2019-12-02 16:28:53 -0300

    Criada a classe de domínio Categoria.

--- src/main/java/com/rsimas/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rsimas/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.rsimas.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rsimas.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String Listar() {
-		return "Rest OK!";
+	public List<Categoria> Listar() {
+
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
