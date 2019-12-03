fc114a9d529d70e2e50468f3ab72095edb18f01a
Ricardo Simas <Rsimas@unigranrio.br>
2019-12-02 21:12:02 -0300

    Criada operação para instanciação do objeto de categoria

--- src/main/java/com/rsimas/cursomc/CursomcApplication.java
+++ src/main/java/com/rsimas/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.rsimas.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rsimas.cursomc.domain.Categoria;
+import com.rsimas.cursomc.repositories.CategoriaRepository;
+
+// Executa uma ação sempre que a aplicação iniciar.
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
