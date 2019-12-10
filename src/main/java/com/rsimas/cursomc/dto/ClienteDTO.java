e173a0c07914de772892e96f479354b64aedf409
Ricardo Simas <Rsimas@unigranrio.br>
2019-12-10 15:13:53 -0300

    Get, Put e Delete para Cliente.

---
+++ src/main/java/com/rsimas/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,55 @@
+package com.rsimas.cursomc.dto;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.rsimas.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@javax.validation.constraints.NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 chars.")
+	private String nome;
+	
+	@javax.validation.constraints.NotEmpty(message="Preenchimento obrigatório")
+	@javax.validation.constraints.Email(message="Email inválido!")
+	private String email;
+	
+	public ClienteDTO() {}
+	
+	public ClienteDTO(Cliente obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
--- src/main/java/com/rsimas/cursomc/services/ClienteService.java
+++ src/main/java/com/rsimas/cursomc/services/ClienteService.java
@@ -1,13 +1,20 @@
 package com.rsimas.cursomc.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
-import com.rsimas.cursomc.domain.Categoria;
 import com.rsimas.cursomc.domain.Cliente;
+import com.rsimas.cursomc.dto.ClienteDTO;
 import com.rsimas.cursomc.repositories.ClienteRepository;
+import com.rsimas.cursomc.services.exception.DataIntregrityException;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
@@ -21,6 +28,49 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new com.rsimas.cursomc.services.exception.ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
+	
+	public Cliente update(Cliente obj) throws ObjectNotFoundException {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) throws ObjectNotFoundException {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntregrityException("Não é possível excluir este Cliente!");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		
+		List<Cliente> categorias = new ArrayList<Cliente>(); 
+		
+		categorias = repo.findAll();
+		
+		return categorias;
+	}
+	
+	//Método que Encapsula informações e operações sobre a paginação. 
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente FromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
 }
