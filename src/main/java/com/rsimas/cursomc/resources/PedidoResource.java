b5cdf065cabd43faa6f6db00394ae5d226a845bb
Ricardo Simas <Rsimas@unigranrio.br>
2019-12-11 21:39:40 -0300

    Implementada inserção de pedido.

--- src/main/java/com/rsimas/cursomc/resources/PedidoResource.java
+++ src/main/java/com/rsimas/cursomc/resources/PedidoResource.java
@@ -1,11 +1,17 @@
 package com.rsimas.cursomc.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rsimas.cursomc.domain.Pedido;
 import com.rsimas.cursomc.services.PedidoService;
@@ -25,5 +31,16 @@ public class PedidoResource {
 		Pedido obj = service.find(id);
 	
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) throws ObjectNotFoundException{
+		obj.setId(null);
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/rsimas/cursomc/services/PedidoService.java
+++ src/main/java/com/rsimas/cursomc/services/PedidoService.java
@@ -1,12 +1,18 @@
 package com.rsimas.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rsimas.cursomc.domain.Categoria;
+import com.rsimas.cursomc.domain.ItemPedido;
+import com.rsimas.cursomc.domain.PagamentoComBoleto;
 import com.rsimas.cursomc.domain.Pedido;
+import com.rsimas.cursomc.domain.enuns.EstadoPagamento;
+import com.rsimas.cursomc.repositories.ItemPedidoRepository;
+import com.rsimas.cursomc.repositories.PagamentoRepository;
 import com.rsimas.cursomc.repositories.PedidoRepository;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -17,10 +23,46 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
 	public Pedido find(Integer id) throws ObjectNotFoundException {
 		Optional<Pedido> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new com.rsimas.cursomc.services.exception.ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Pedido insert(Pedido obj) throws ObjectNotFoundException{
+		obj.setId(null);
+		obj.setInstante(new Date());
+		
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
+		
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
+	}
 }
