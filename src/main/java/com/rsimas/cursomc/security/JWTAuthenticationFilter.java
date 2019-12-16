36063e4b741175a1a13ea926761629a1d75d4ca4
Ricardo Simas <Rsimas@unigranrio.br>
2019-12-16 20:46:54 -0300

    Implementando autorização.

--- src/main/java/com/rsimas/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/rsimas/cursomc/security/JWTAuthenticationFilter.java
@@ -2,14 +2,18 @@ package com.rsimas.cursomc.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -45,9 +49,26 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	@Override
 	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
 			Authentication auth) {
-		
+
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
-        String token = jwtUtil.generateToken(username);
-        res.addHeader("Authorization", "Bearer " + token);
+		String token = jwtUtil.generateToken(username);
+		res.addHeader("Authorization", "Bearer " + token);
+	}
+
+	public class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+		@Override
+		public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+				AuthenticationException exception) throws IOException, ServletException {
+			response.setStatus(401);
+			response.setContentType("application/json");
+			response.getWriter().append(json());
+		}
+
+		private String json() {
+			long date = new Date().getTime();
+			return "{\"timestamp\": " + date + ", " + "\"status\": 401, " + "\"error\": \"Não autorizado\", "
+					+ "\"message\": \"Email ou senha inválidos\", " + "\"path\": \"/login\"}";
+		}
 	}
 }
