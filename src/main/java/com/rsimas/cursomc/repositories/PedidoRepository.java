e465595077e639050769565fcc8d3ee786f87e51
Ricardo Simas <Rsimas@unigranrio.br>
2019-12-17 15:08:47 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos.

--- src/main/java/com/rsimas/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/rsimas/cursomc/repositories/PedidoRepository.java
@@ -1,12 +1,15 @@
 package com.rsimas.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.rsimas.cursomc.domain.Cliente;
 import com.rsimas.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 	
-	
+	@org.springframework.transaction.annotation.Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, org.springframework.data.domain.Pageable pageRequest);
 }
--- src/main/java/com/rsimas/cursomc/services/PedidoService.java
+++ src/main/java/com/rsimas/cursomc/services/PedidoService.java
@@ -4,9 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
-import com.rsimas.cursomc.domain.Categoria;
+import com.rsimas.cursomc.domain.Cliente;
 import com.rsimas.cursomc.domain.ItemPedido;
 import com.rsimas.cursomc.domain.PagamentoComBoleto;
 import com.rsimas.cursomc.domain.Pedido;
@@ -14,6 +17,8 @@ import com.rsimas.cursomc.domain.enuns.EstadoPagamento;
 import com.rsimas.cursomc.repositories.ItemPedidoRepository;
 import com.rsimas.cursomc.repositories.PagamentoRepository;
 import com.rsimas.cursomc.repositories.PedidoRepository;
+import com.rsimas.cursomc.security.UserSS;
+import com.rsimas.cursomc.services.exception.AuthorizationException;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
@@ -45,7 +50,7 @@ public class PedidoService {
 		Optional<Pedido> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new com.rsimas.cursomc.services.exception.ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 	
 	public Pedido insert(Pedido obj) throws ObjectNotFoundException{
@@ -74,4 +79,20 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
+	
+	//Método que Encapsula informações e operações sobre a paginação. 
+		public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) throws ObjectNotFoundException{
+			
+			UserSS user = UserService.authenticated();
+			if(user == null) {
+				throw new AuthorizationException("Acesso Negado!");
+			}
+			
+			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+					orderBy);
+			
+			Cliente cliente = clienteService.find(user.getId());
+			
+			return repo.findByCliente(cliente, pageRequest);
+		}
 }
