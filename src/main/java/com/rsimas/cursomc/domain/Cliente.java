dc2fab4d0a985739804d9efb172c66c5a82bb7df
Ricardo Simas <Rsimas@unigranrio.br>
2019-12-18 18:45:23 -0300

    Salvando URL da imagem em cliente.

--- src/main/java/com/rsimas/cursomc/domain/Cliente.java
+++ src/main/java/com/rsimas/cursomc/domain/Cliente.java
@@ -52,7 +52,9 @@ public class Cliente implements Serializable{
 	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
-
+	
+	private String imageUrl;
+	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -146,6 +148,14 @@ public class Cliente implements Serializable{
 
 	public void setPedidos(List<Pedido> pedidos) {
 		this.pedidos = pedidos;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
 	}
 
 	@Override
